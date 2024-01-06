8d121f332fdef249db2fef102917f8d05a8a0a29
Jonathan Huang <jonathannhuang@gmail.com>
2024-01-06 15:16:00 -0500

    Initial Commit

---
+++ Call Recorder/Call_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Call_RecorderApp.swift
+//  Call Recorder
+//
+//  Created by Jonathan Huang on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Call_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Call RecorderUITests/Call_RecorderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Call_RecorderUITestsLaunchTests.swift
+//  Call RecorderUITests
+//
+//  Created by Jonathan Huang on 1/6/24.
+//
+
+import XCTest
+
+final class Call_RecorderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
